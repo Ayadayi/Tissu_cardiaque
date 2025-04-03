17d996bd4d8eb44ba87fa78a5f6ebb39c30f7ac1
radia0203 <lemraniradia@gmail.com>
2025-04-03 13:56:33 +0200

    V1.1
    affiche les points d'une courbe

--- courbe/dessinateur_points.gd
+++ courbe/dessinateur_points.gd
@@ -1,28 +1,44 @@
 extends Control
 
-# Liste des points de la courbe en coordonnées normales (0.0 à 1.0)
-var points_normaux = [
-	Vector2(0.1, 0.2),
-	Vector2(0.3, 0.5),
-	Vector2(0.5, 0.3),
-	Vector2(0.7, 0.8),
-	Vector2(0.9, 0.6)
+# Paramètres pour la sinusoïde
+var amplitude = 100
+var fréquence = 0.05
+var phase = 0
+
+# Tableau pour stocker les points sinusoïdaux (en dur)
+var points_sinusoidaux = [
+	Vector2(0, 300),
+	Vector2(50, 280),
+	Vector2(100, 250),
+	Vector2(150, 230),
+	Vector2(200, 210),
+	Vector2(250, 180),
+	Vector2(300, 150),
+	Vector2(350, 120),
+	Vector2(400, 100),
+	Vector2(450, 120),
+	Vector2(470, 140),
+	Vector2(500, 180),
+	Vector2(550, 140),
+	Vector2(600, 130),
+	Vector2(650, 110),
+	Vector2(700, 80),
+	Vector2(750, 60),
+	Vector2(800, 70),
+	Vector2(880, 80)
 ]
 
 func _ready():
-	# Redessine lorsque la taille change
-	connect("resized", Callable(self, "_on_resized"))
+	# Mettre à jour les points avec les valeurs sinusoïdales
+	for i in range(points_sinusoidaux.size()):
+		var x = points_sinusoidaux[i].x
+		var y = amplitude * sin(fréquence * x + phase) + 300  # Calculer la valeur Y en fonction de la sinusoïde
+		points_sinusoidaux[i] = Vector2(x, y)  # Mettre à jour la position Y de chaque point
+
+	# Demander un redessin
 	queue_redraw()
 
 func _draw():
-	# Obtenir la taille actuelle du Control (qui correspond à celle du TextureRect)
-	var taille = get_size()
-	for point_normal in points_normaux:
-		# Convertir les coordonnées normales en coordonnées locales
-		var point_local = Vector2(point_normal.x * taille.x, point_normal.y * taille.y)
-		draw_circle(point_local, 5, Color(1, 0, 0))  # Dessine un cercle rouge de rayon 5
-
-func _on_resized():
-	# Redessine les points lorsque la taille change
-	queue_redraw()
-	### comment 
+	# Dessiner les points sans les relier
+	for point in points_sinusoidaux:
+		draw_circle(point, 5, Color(1, 0, 0))  # Afficher chaque point sous forme de cercle rouge de rayon 5
