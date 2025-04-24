074adbce13e5a8c05db62dafa9d803c5162af384
Ayad <167223214+Ayadayi@users.noreply.github.com>
2025-04-24 13:19:24 +0200

    importButton

--- courbe/button.gd
+++ courbe/button.gd
@@ -1,1 +1,16 @@
 extends Button
+@onready var file_dialog = $FileImporter
+
+func _pressed():
+	var file_dialog = get_parent().get_node("FileImporter")
+	file_dialog.popup_centered()
+	
+func _on_FileImporter_file_selected(path):
+	print("Fichier importé :", path)
+	# Tu peux ici charger le fichier selon son type
+	# Par exemple, pour une image :
+	var texture = ImageTexture.new()
+	var img = Image.new()
+	img.load(path)
+	texture.create_from_image(img)
+	$TextureRect.texture = texture
