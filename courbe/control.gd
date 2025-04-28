e6ddf8bc95043353f7a60b31a387908703398788
radia0203 <lemraniradia@gmail.com>
2025-04-28 15:35:04 +0200

    update

--- courbe/control.gd
+++ courbe/control.gd
@@ -1,15 +1,12 @@
 extends Control
 
 const POINT_SIZE := 3
-var width := 600
+var width := 900
 var height := 400
 
 var points : Array = []
-var times_real: Array = []
-
 
 func _ready():
-	# Ajouter FileDialog si pas dans l'éditeur
 	var file_dialog := FileDialog.new()
 	file_dialog.name = "FileDialog"
 	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
@@ -18,18 +15,32 @@ func _ready():
 	add_child(file_dialog)
 	file_dialog.file_selected.connect(self._on_fichier_selectionne)
 
-	
 
 	queue_redraw()
 
+	############################# afficher les mesures #############################
 
+func afficher_mesures():
+	var amplitude = get_amplitude()
+	print("Amplitude de contraction : ", amplitude)
 
+	var contraction_time = get_contraction_time()
+	print("Temps de contraction : ", contraction_time, " ms")
+
+	var decontraction_time = get_decontraction_time()
+	print("Temps de décontraction : ", decontraction_time, " ms")
+
+	var vitesse_contraction = get_contraction_speed_percent(20, 80)
+	print("Vitesse de contraction (20%-80%) : ", vitesse_contraction)
+
+	var vitesse_decontraction = get_decontraction_speed_percent(50, 10)
+	print("Vitesse de décontraction (50%-10%) : ", vitesse_decontraction)
+	
 
 ####################### Quand on clique sur le bouton #######################
 func _on_ouvrir_fichier_pressed():
 	var file_dialog = get_node("FileDialog")
 	file_dialog.popup_centered()
-
 
 ######################## Quand un fichier est sélectionné #######################
 func _on_fichier_selectionne(path: String):
@@ -44,17 +55,15 @@ func _on_fichier_selectionne(path: String):
 	var step := 1
 	points.clear()
 
-	# Lire une fois pour compter les lignes
 	while not file.eof_reached():
 		file.get_line()
 		total_lignes += 1
 	file.close()
 
-	step = max(total_lignes / 400, 1)
+	step = max(total_lignes / 50, 1)
 	print("Nombre total de lignes : ", total_lignes)
 	print("Step calculé : ", step)
 
-	# Relire pour collecter les points
 	file = FileAccess.open(path, FileAccess.READ)
 	var count := 0
 
@@ -70,24 +79,14 @@ func _on_fichier_selectionne(path: String):
 			if parts.size() >= 4:
 				var x = float(parts[0])
 				var y = float(parts[3])
-				#print("Y brut :", y)
-				#print("X brut :", x)     # Ajoute cette ligne
 
-
-				# Mettre à jour les min/max pour x et y
 				min_x = min(min_x, x)
 				max_x = max(max_x, x)
 				min_y = min(min_y, y)
 				max_y = max(max_y, y)
-
 		count += 1
 	file.close()
 
-	# Normalisation de X (étaler les points sur toute la largeur)
-	#print("min_x = ", min_x, " max_x = ", max_x)
-	#print("min_y = ", min_y, " max_y = ", max_y)
-
-	# Relire encore une fois avec les points
 	file = FileAccess.open(path, FileAccess.READ)
 	count = 0
 
@@ -96,32 +95,27 @@ func _on_fichier_selectionne(path: String):
 		if count % step == 0 and line != "":
 			var parts := line.split("\t")
 			if parts.size() >= 4:
-				var x_raw = float(parts[0]) # temps en ms
-				var y_raw = float(parts[3]) # reponse du muscle
+				var x_raw = float(parts[0])
+				var y_raw = float(parts[3])
 
-				# Mettre à l’échelle X et Y
 				var x_scaled = 50 + ((x_raw - min_x) / (max_x - min_x) * (width - 50))
-  # Adapter X à la largeur
-				var y_scaled = height - ((y_raw - min_y) / (max_y - min_y) * height)  # Adapter Y à la hauteur
+				var y_scaled = height - ((y_raw - min_y) / (max_y - min_y) * height)
 
 				points.append(Vector2(x_scaled, y_scaled))
 		count += 1
 	file.close()
 
 	print("Nombre de points affichés : ", points.size())
+	
+	if points.size() > 0:
+		afficher_mesures()
+
 	queue_redraw()
 
-
-<<<<<<< Updated upstream
-
-
-=======
 ########################################  draw  ##########################################################
->>>>>>> Stashed changes
-
 func _draw():
-	draw_line(Vector2(50, 0), Vector2(50, height), Color.NAVY_BLUE, 2)  # Axe Y
-	draw_line(Vector2(0, height), Vector2(width, height), Color.NAVY_BLUE, 2)  # Axe X
+	draw_line(Vector2(50, 0), Vector2(50, height), Color.NAVY_BLUE, 2)
+	draw_line(Vector2(0, height), Vector2(width, height), Color.NAVY_BLUE, 2)
 
 	for point in points:
 		draw_circle(point, POINT_SIZE, Color.BLACK)
@@ -129,27 +123,35 @@ func _draw():
 	for i in range(points.size() - 1):
 		draw_line(points[i], points[i + 1], Color.BLACK, 2)
 
-	draw_circle(get_max_point(), POINT_SIZE + 2, Color(0, 1, 0))
-	draw_circle(get_min_point(), POINT_SIZE + 2, Color(1, 0, 0))
+	draw_circle(get_max_point(), POINT_SIZE + 2, Color(0, 1, 0)) # Pic contraction
+	draw_circle(get_min_point(), POINT_SIZE + 2, Color(1, 0, 0)) # Repos
 
+##########################################
+func get_start_of_contraction() -> int:
+	for i in range(1, points.size()):
+		if points[i].y < points[i - 1].y:
+			return i - 1
+	return 0
 
 ########################################  Min et Max  ##########################################################
-#la point le plus haut
+#le point le plus haut
 func get_max_point() -> Vector2:
 	if points.size() == 0:
-		return Vector2.ZERO  # Ou une autre valeur par défaut
-	var max_point = points[0]
-	for p in points:
-		if p.y < max_point.y:
-			max_point = p
+		return Vector2.ZERO
+
+	var start_index = get_start_of_contraction()
+	var max_point = points[start_index]
+	for i in range(start_index, points.size()):
+		if points[i].y < max_point.y:
+			max_point = points[i]
 	return max_point
 
-	
 
 #le point le plus bas 
 func get_min_point() -> Vector2:
 	if points.size() == 0:
-		return Vector2.ZERO  # Ou une autre valeur par défaut
+		return Vector2.ZERO
+
 	var min_point = points[0]
 	for p in points:
 		if p.y > min_point.y:
@@ -159,39 +161,25 @@ func get_min_point() -> Vector2:
 
 #######################################  L'amplitude Max  #########################################################
 func get_amplitude() -> float:
-	if points.size() == 0:
-		return 0.0  # Ou une autre valeur par défaut
 	var base_y = height
 	var max_y = get_max_point().y
 	return base_y - max_y
-
 
 ########################################  Duree de Contraction ##################################################
 func get_contraction_time() -> float:
 	if points.size() < 2:
 		return 0
-	var start_time := -1.0
-	var max_point := get_max_point()
+	var start_time = points[get_start_of_contraction()].x
+	var peak_time = get_max_point().x
+	return peak_time - start_time
 
-	for i in range(1, points.size()):
-		if points[i].y < points[i - 1].y:
-			start_time = points[i - 1].x  # Prendre le x du dernier point plat (fin de la latence)
-			break
-
-	if start_time == -1:
-		return 0
-	return max_point.x - start_time
-
-
-######################################  Duree de Decontraction ##################################################
+########################################  Duree de decontraction ##################################################
 func get_decontraction_time() -> float:
 	if points.size() < 2:
 		return 0
-
-	var max_point := get_max_point()
-	var end_time: float = points[points.size() - 1].x
-
-	return end_time - max_point.x
+	var peak_time = get_max_point().x
+	var end_time = points[points.size() - 1].x
+	return end_time - peak_time
 
 
 ######################################  Vitesse de Contraction ##################################################
@@ -199,8 +187,8 @@ func get_contraction_speed_percent(x_percent: float, y_percent: float) -> float:
 	if points.size() < 2:
 		return 0
 
-	var y_rest = points[0].y              # niveau de repos
-	var y_peak = get_max_point().y        # sommet (force max)
+	var y_rest = points[0].y
+	var y_peak = get_max_point().y
 	var amplitude = y_rest - y_peak
 
 	var y1 = y_rest - (x_percent / 100.0) * amplitude
@@ -215,8 +203,7 @@ func get_contraction_speed_percent(x_percent: float, y_percent: float) -> float:
 
 	for p in points:
 		if p.x > max_point.x:
-			continue  # on reste dans la montée
-
+			continue
 		var diff1 = abs(p.y - y1)
 		if diff1 < min_diff1:
 			min_diff1 = diff1
@@ -236,14 +223,14 @@ func get_contraction_speed_percent(x_percent: float, y_percent: float) -> float:
 	return abs(delta_force / delta_time)
 
 
-######################################  Vitesse de Décontraction ##################################################
+######################################  Vitesse de decontraction ##################################################
 func get_decontraction_speed_percent(x_percent: float, y_percent: float) -> float:
 	if points.size() < 2:
 		return 0.0
 
-	var y_rest = points[0].y               # niveau de repos
-	var y_peak = get_max_point().y         # sommet (force max)
-	var amplitude = y_rest - y_peak        # amplitude toujours positive
+	var y_rest = points[0].y
+	var y_peak = get_max_point().y
+	var amplitude = y_rest - y_peak
 
 	var y1 = y_peak + (x_percent / 100.0) * amplitude
 	var y2 = y_peak + (y_percent / 100.0) * amplitude
@@ -257,8 +244,7 @@ func get_decontraction_speed_percent(x_percent: float, y_percent: float) -> floa
 
 	for p in points:
 		if p.x <= max_point.x:
-			continue  # on ignore la phase de contraction (avant ou au pic)
-
+			continue
 		var diff1 = abs(p.y - y1)
 		if diff1 < min_diff1:
 			min_diff1 = diff1
@@ -277,10 +263,5 @@ func get_decontraction_speed_percent(x_percent: float, y_percent: float) -> floa
 
 	return abs(delta_force / delta_time)
 
-
-#func _on_button_pressed() -> void:
-	#pass # bouton inutilisé ici pour l’instant
-
-
 func _on_button_pressed() -> void:
-	pass # Replace with function body.
+	pass
